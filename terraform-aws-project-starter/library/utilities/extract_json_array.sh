ffed5c7d11ae07f969b3c8dc1bc88bd889f370c4
Joshua Uzzell <josh.uzzell@gmail.com>
2024-05-08 12:14:37 -0400

    Remove terraform starter project

--- terraform-aws-project-starter/library/utilities/extract_json_array.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-extract_json_array() {
-    key=$1
-    config_file=$2
-    grep -A 1 "\"$key\"" "$config_file" | grep -v "\"$key\"" | sed -E 's/[^"]*"([^"]+)".*/\1/g'
-}
--- terraform-aws-project-starter/library/utilities/extract_json_value.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-extract_json_value() {
-    key=$1
-    config_file=$2
-    grep "\"$key\"" "$config_file" | sed -E 's/.*: *"([^"]+)".*/\1/'
-}
